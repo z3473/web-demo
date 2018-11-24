60cbdf46770fe32561a5160b16186cd6382b7880
z3473 <965321009@qq.com>
2018-11-24 17:32:14 +0800

    v3.0

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
- <?php
-      $link = mysql_connect('192.168.141.12','root','123456');
-      if ($link)
-        echo "Success...";
-      else
-        echo "Failure...";
-
-      mysql_close();
-       123456
-    ?>
-
+<?php
+phpinfo();
+?>
